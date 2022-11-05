523467115fe675dd18011d708517dea11c26ab99
ViktorPyrih <v24102002>
2022-11-06 00:31:54 +0200

    Implemented fork tasks

--- lab1/task1/main.c
+++ lab1/task1/main.c
@@ -1,14 +1,26 @@
 #include "stdio.h"
 #include "unistd.h"
+#include "sys/wait.h"
 
 int main() {
-    int pid = fork();
-    if (pid == 0) {
-        printf("child %d\n", getpid());
-        sleep(10);
+    int pid0 = fork();
+
+    if (pid0 == 0) {
+        fork();
+        sleep(30);
     } else {
-        printf("parent %d\n", getpid());
-        sleep(10);
+        int pid2 = fork();
+
+        if (pid2 == 0) {
+            int pid3 = fork();
+            if (pid3 == 0) {
+                fork();
+            }
+            sleep(30);
+        } else {
+            fork();
+            sleep(30);
+        }
     }
     return 0;
 }
--- lab1/task2/main.c
+++ lab1/task2/main.c
@@ -1,15 +1,32 @@
 #include "sys/wait.h"
 #include "stdio.h"
 #include "unistd.h"
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    char* arguments = extractArguments(argc, argv);
     int pid = fork();
     if (pid == 0) {
-        printf("child %d\n", getpid());
-        char *argv[3] = {"echo", "Hey!", NULL};
-        execvp("echo", argv);
+        execvp(argv[1], arguments);
+        exit(1);
     } else {
-        printf("parent %d\n", getpid());
+        waitpid(pid, &status, 0);
+        if (status == 0) {
+            printf("Success!\n");
+        } else {
+            printf("Failed, exit code = %d\n", status);
+        }
     }
+
     return 0;
+}
+
+char* extractArguments(int argc, char *argv[]) {
+    char* arguments[argc];
+    for (int i = 1; i < argc; i++) {
+        arguments[i - 1] = argv[i];
+    }
+    arguments[argc - 1] = NULL;
+
+    return arguments;
 }
